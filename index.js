5a3982bc1e8867c0f848d7e333d3954691559f54
James Sampson <james.sampson@bluefindata.com>
2019-12-15 17:13:07 -0600

    feat: initial commit with access token tag

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports.templateTags = [require('./src/access-token')];
---
+++ src/access-token.js
@@ -0,0 +1,47 @@
+module.exports = {
+  name: 'token',
+  displayName: 'Access Token',
+  description: "reference access token from other requests",
+  args: [
+    {
+      displayName: 'Request',
+      type: 'model',
+      model: 'Request',
+    }
+  ],
+
+  async run(context, oauthRequestId) {
+    const { meta } = context;
+
+    if (!meta.requestId || !meta.workspaceId) {
+      return null;
+    }
+
+    if (!oauthRequestId) {
+      throw new Error('No request specified');
+    }
+
+    const authenticationRequest = await context.util.models.request.getById(oauthRequestId);
+    const prefix = ((authenticationRequest || {}).authentication || {}).tokenPrefix;
+
+    const token = await context.util.models.oAuth2Token.getByRequestId(authenticationRequest._id);
+    const accessToken = (token || {}).accessToken || '';
+
+    if (context.renderPurpose == null) {
+      return `${prefix} ${accessToken || "<access-token-pending>"}`.trim();
+    }
+
+    if (!accessToken) {
+      await context.app.alert("Access Token", "The access token is missing");
+
+      return '';
+    }
+    else if (token.expiresAt < new Date()) {
+      await context.app.alert("Access Token", "The access token has expired");
+
+      return '';
+    }
+
+    return `${prefix} ${accessToken}`.trim();
+  }
+};
